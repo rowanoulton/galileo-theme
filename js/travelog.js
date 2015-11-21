ea0ec838af26f91802bdd1c5f45ecca85dea2fee
Rowan Oulton <rowan.oulton@gmail.com>
2015-11-21 19:03:21 +0100

    Refactor JS

--- js/travelog.js
+++ js/travelog.js
@@ -1,19 +1,16 @@
 var secretRevealed = false
+var doSecretReveal
+var resetSecretReveal
 
-document.addEventListener('DOMContentLoaded', function () {
-  var revealSecret = function () {
-    // Remove the negative offset from the banner, while also scrolling down the page
-    // this reveals the secret without affecting the users position on the page
-    document.body.className = document.body.className.replace('is-offset', '')
-    window.scroll(0, window.scrollY + 195)
-    secretRevealed = true
-  }
+doSecretReveal = function () {
+  // Remove the negative offset from the banner, while also scrolling down the page
+  // this reveals the secret without affecting the users position on the page
+  document.body.className = document.body.className.replace('is-offset', '')
+  window.scroll(0, window.scrollY + 195)
+  secretRevealed = true
+}
 
-  // There must be at least some delay here, otherwise it doesn't seem to play ball
-  setTimeout(revealSecret, 3000)
-})
-
-window.onbeforeunload = function () {
+resetSecretReveal = function () {
   if (secretRevealed) {
     // The native browser behaviour of automatically scrolling to a users last position on refresh
     // is upset by our revealing of the secret banner. This meant that, on refresh, the position
@@ -23,3 +20,13 @@ window.onbeforeunload = function () {
     window.scroll(0, Math.max(window.scrollY - 195, 0))
   }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  // 3000 ms is arbitrary, though there must be at least some
+  // delay here, otherwise it doesn't seem to play ball
+  setTimeout(doSecretReveal, 3000)
+})
+
+window.addEventListener('beforeunload', function () {
+  resetSecretReveal()
+})
